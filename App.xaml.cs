fad3d5021085056822408094a614666faae17fc0
Adam Kędryna <adam.kedryna@gmail.com>
2026-09-11 18:43:09 +0200

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Gra_w_kosci_2026_desktop
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Gra_w_kosci_2026_desktop
+{
+    public partial class MainWindow : Window
+    {
+        public Kosc[] Kosci = {
+            new Kosc(0),
+            new Kosc(0),
+            new Kosc(0),
+            new Kosc(0),
+            new Kosc(0),
+        };
+       
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Rzut(object sender, RoutedEventArgs e)
+        {
+            foreach (Kosc k in Kosci)
+            {
+                k.RzutKoscia();
+
+            }
+        }
+    }
+}
